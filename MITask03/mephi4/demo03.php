30d451179614320c5b1dd17b8022b4ee12a683df
MI <Coffe731@yandex.ru>
2021-09-24 22:18:50 +0300

    Task03

---
+++ MITask03/mephi4/demo03.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+		<title>Demo</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<h1>Первый взгляд на PHP</h1>
+		<?php
+		
+			$x = 2;
+			$y = 3;
+			$z = $x + $y;
+			//echo("Result: $z");
+			date_default_timezone_set ("Europe/Moscow");
+			//date_default_timezone_set ("America/Los_Angeles");
+			//date_default_timezone_set ("Asia/Hong_Kong");
+			$now = date("H:i:s");
+			//echo("Время открытия страницы: $now</h2>");
+			$greeting = "Добрый вечер!";
+			$hour = date("H");
+			if ($hour<5) {
+				$greeting = "Доброй ночи!";
+			echo '<img src="images/4.jpg"   width="400" height="300">';
+				}
+			if ($hour>=5 and $hour<12) {
+				$greeting = "Доброе утро!";
+				echo '<img src="images/1.jpg"   width="400" height="300">';
+				}
+			if ($hour>=12 and $hour<18) {
+				$greeting = "Добрый день!";
+			echo '<img src="images/2.jpg"   width="400" height="300">';
+				}
+			if ($hour>=18) {
+			echo '<img src="images/3.jpg"   width="400" height="300">';
+			}
+				//$greeting = "Добрый вечер!";
+				
+			
+			
+			
+		?>
+		<h2> Время открытия страницы: <?=$now?> ! </h2>
+		<h1> <?=$greeting ?> </h1>
+		
+	</body>
+
+</html>
---
+++ MITask03/mephi4/doubler.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title>Удвоитель</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<h1>Мы удвоим ваше число!</h1>
+		<?php
+			$amount = "";
+			if(isset($_REQUEST["data"]))
+				$amount = $_REQUEST["data"];
+			
+			
+		?>
+		
+		<form>
+		
+			<input type="number" name ="data" value =" <?=$amount?>"/>
+			<input type="submit" value = "Попробуйте!"/>
+			
+		</form>
+		<?php
+			if(isset($_REQUEST["data"]))
+			$amount *= 2;
+			echo("<h3>Вашe новое число: $amount</h3>");
+			
+			
+		?>
+		
+
+	</body>
+
+</html>
