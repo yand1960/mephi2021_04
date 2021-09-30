e32b532ddf3836c0e73a4f0121bb6f7b598b2c13
Tim <tim9728@mail.ru>
2021-09-30 21:09:38 +0300

    дз обнова

--- TSTask03/demo03.php
+++ TSTask03/demo03.php
@@ -1,58 +1,44 @@
 <html>
-  <head>
-    <title>Demo</title>
-    <meta charset="utf-8" />
-    
-  </head>
-  <body>
-    <h1>Первый взгляд на PHP</h1>
-    <?php
-      
-      $x = 2;
-      $y = 3;
-      $z = $x + $y;
-      //echo("Result: $z");
-      
-      date_default_timezone_set("Europe/Moscow");
-      //date_default_timezone_set("America/Los_Angeles");
-      //date_default_timezone_set("Asia/Hong_Kong");
-      
-      $now = date("H:i:s");
-      //echo("<h2>Время открытия страницы: $now</h2>");
-      
-      // Дефолтное значение "на всякий случай"
-      $picture = "base_image.jpg";
-      
-      $hour = date("H");
-      if ($hour < 5) {
-        $greeting = "Доброй ночи!";
-        $picture = "дн.jpg";
-      }
-        
-      
-      if ($hour >= 5 and $hour < 12) {
-        $greeting = "Доброе утро!";
-        $picture = "ду.jpg";
-      }
-        
-      
-      if ($hour >= 12 and $hour < 18) {
-        $greeting = "Добрый день!";
-        $picture = "дд.jpg";
-      }
-
-        
-      if ($hour >= 18) {
-        $greeting = "Добрый вечер!";
-        $picture = "дв.jpg";
-      }
-      
-      // Отображение картинки c базовыми параметрами ширины и высоты
-      $playfile = "/upload/".$picture;
-      echo('<img src="<?php echo $playfile; ?>"  alt="day image" width="800" height="600">');
-                    
-    ?>
-    <h2>Время открытия страницы: <?=$now?>!</h2>
-    <h1><?=$greeting?></h1>
-  </body>
+	<head>
+		<title>Demo</title>
+		<meta charset="utf-8"/>
+		
+	</head>
+	<body>
+		<h1 id="hdr1">Первый взгляд на PHP</h1>
+		<?php
+		
+			$x = 2;
+			$y = 3;
+			$z = $x + $y;
+			//echo("Result: $z");
+			
+			date_default_timezone_set("Europe/Moscow");
+			
+			$now = date("H:i:s");
+			//echo("<h2>Время открытия страницы: $now</h2>");
+			$hour = date("H");
+			$greeting = "Добрый вечер!";
+			if ($hour<5) 
+			{
+				$greeting = "Доброй ночи!";
+				echo "<img width='500' src='images/дн.jpg'>";
+				}
+			if ($hour>=5 and $hour<12) {
+				$greeting = "Доброе утро!";
+				echo "<img width='500' src='images/ду.jpg'>";
+				}
+			if ($hour>=12 and $hour<18) {
+				$greeting = "Добрый день!";
+				echo "<img width='500' src='images/дд.jpg'>";
+				}
+			if ($hour>=18)
+				echo "<img width='500' src='images/дв.jpg'>";
+				$greeting = "Добрый вечер!";
+			
+		?>
+		<h3>Время открытия страницы: <?=$now?>! </h3>
+		<h4><?=$greeting?></h4>
+		
+	</body>
 </html>
