c81458da266324c49446fc1dbb9697c2d7e3c4e1
Tim <tim9728@mail.ru>
2021-10-08 18:59:42 +0300

    5

--- TSTask05/TSTask05.php
+++ TSTask05/TSTask05.php
@@ -30,20 +30,27 @@
       $now = date("H:i:s");
       //echo("<h2>Время открытия страницы: $now</h2>");
       $hour = date("H");
-      if ($hour < 5)
-        $greeting = "Доброй ночи!";
-      
-      if ($hour >= 5 and $hour < 12)
-        $greeting = "Доброе утро!";
-      
-      if ($hour >= 12 and $hour < 18)
-        $greeting = "Добрый день!";
-        
-      if ($hour >= 18)
-        $greeting = "Добрый вечер!";
-            
-    ?>
-    <h2>Время открытия страницы: <?=$now?>!</h2>
-    <h1><?=$greeting?></h1>
-  </body>
+			$greeting = "Добрый вечер!";
+			if ($hour<5) 
+			{
+				$greeting = "Доброй ночи!";
+				echo "<img width='500' src='images/дн.jpg'>";
+				}
+			if ($hour>=5 and $hour<12) {
+				$greeting = "Доброе утро!";
+				echo "<img width='500' src='images/ду.jpg'>";
+				}
+			if ($hour>=12 and $hour<18) {
+				$greeting = "Добрый день!";
+				echo "<img width='500' src='images/дд.jpg'>";
+				}
+			if ($hour>=18)
+				echo "<img width='500' src='images/дв.jpg'>";
+				$greeting = "Добрый вечер!";
+			
+		?>
+		<h3>Время открытия страницы: <?=$now?>! </h3>
+		<h4><?=$greeting?></h4>
+		
+	</body>
 </html>
