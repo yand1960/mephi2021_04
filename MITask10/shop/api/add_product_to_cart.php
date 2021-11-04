ba71549971e3ccbc33e660da863606baa17ea413
MI <Coffe731@yandex.ru>
2021-11-04 23:18:28 +0300

    shops

---
+++ MITask10/shop/api/add_product_to_cart.php
@@ -0,0 +1,17 @@
+<?php
+// В PHP ссеию нужно стратовать чтобы мы хотим чтобы страница началась
+session_start();
+
+$id = $_REQUEST["id"];
+$qty = $_REQUEST["qty"];
+
+$car = [];
+// Не хранится ли в сессии корзина? Если да, прочитать ее
+if (isset($_SESSION["cart"]))
+    $cart = $_SESSION["cart"];
+// Добавляем к корзине(массив) новый товар 
+$cart[] = [$id, $qry];
+//Сохраняем корзину в сессии на сервере
+$_SESSION["cart"] = $cart;
+
+echo(json_encode($cart));
