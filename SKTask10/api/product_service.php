daf88e283733bd2329e846665d00429ee1a1cbc1
stanislava <stasikylak@mail.ru>
2021-11-11 11:54:20 +0300

    shop

---
+++ SKTask10/api/product_service.php
@@ -0,0 +1,18 @@
+<?php
+
+header("Content-Type: application/json; charset=UTF-8");
+
+include("../../shop_params.php");
+
+$conn = mysqli_connect(SERVER_URL,DB_USER,DB_PWD,DB_NAME);
+
+$sql = "SELECT * FROM products";
+$stmt = mysqli_prepare($conn,$sql);
+mysqli_stmt_execute($stmt);
+$cursor = mysqli_stmt_get_result($stmt);
+$products = mysqli_fetch_all($cursor);
+
+//var_dump($courses);
+echo(json_encode($products, JSON_UNESCAPED_UNICODE));
+
+mysqli_close($conn);
